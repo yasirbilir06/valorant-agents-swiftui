028e9aadf72229e8c6ce5b0581f31ab38c5e8c33
yasirbilir06 <yasiryenihasan@gmail.com>
2026-01-15 20:03:03 +0100

    Initial Commit

---
+++ SuperHeroBook/SuperHeroBookApp.swift
@@ -0,0 +1,17 @@
+//
+//  SuperHeroBookApp.swift
+//  SuperHeroBook
+//
+//  Created by Yasir Bilir on 15.01.2026.
+//
+
+import SwiftUI
+
+@main
+struct SuperHeroBookApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
